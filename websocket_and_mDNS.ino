fbee97d5432953fec312852f80c4a0e0e41383fe
Echoskope <red.rose.audio@gmail.com>
2023-08-21 19:05:58 -0500

    Initial Commit

---
+++ websocket_and_mDNS.ino
@@ -0,0 +1,100 @@
+#include <WiFi.h>
+#include <WebSocketsServer.h>
+#include <ESPmDNS.h>
+#include <WiFiClient.h>
+#include "arduino_secrets.h"
+
+
+// Constants
+const char* ssid = SECRET_SSID; // Defined in arduino_secrets.h
+const char* password = SECRET_PASS; // Defined in arduino_secrets.h
+
+// Globals
+WebSocketsServer webSocket = WebSocketsServer(80);
+
+// Called when receiving any WebSocket message
+void onWebSocketEvent(uint8_t num,
+                      WStype_t type,
+                      uint8_t * payload,
+                      size_t length) {
+
+  // Figure out the type of WebSocket event
+  switch(type) {
+
+    // Client has disconnected
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+
+    // New client has connected
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connection from ", num);
+        Serial.println(ip.toString());
+      }
+      break;
+
+    // Echo text message back to client
+    case WStype_TEXT:
+      Serial.printf("[%u] Text: %s\n", num, payload);
+      webSocket.sendTXT(num, payload);
+      break;
+
+    // For everything else: do nothing
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    default:
+      break;
+  }
+}
+
+void setup() {
+
+  // Start Serial port
+  Serial.begin(115200);
+
+  // Connect to access point
+  Serial.println("Connecting");
+  WiFi.begin(ssid, password);
+  
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print our IP address
+  Serial.println("Connected!");
+  Serial.print("My IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start WebSocket server and assign callback
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+    // Set up mDNS responder:
+    // - first argument is the domain name, in this example
+    //   the fully-qualified domain name is "esp32.local"
+    // - second argument is the IP address to advertise
+    //   we send our IP address on the WiFi network
+    if (!MDNS.begin("esp32")) {
+        Serial.println("Error setting up MDNS responder!");
+        while(1) {
+            delay(1000);
+        }
+    }
+    Serial.println("mDNS responder started");
+
+        // Add service to MDNS-SD
+    MDNS.addService("http", "tcp", 80);
+
+}
+
+void loop() {
+
+  // Look for and handle WebSocket data
+  webSocket.loop();
+}
