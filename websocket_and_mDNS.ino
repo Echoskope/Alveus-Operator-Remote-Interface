f14a11d5676d3ea6c8efaa7eca24cc425cfbd14f
Echoskope <red.rose.audio@gmail.com>
2023-09-03 17:08:59 -0500

    refactored for neopixel on S3 board

--- websocket_and_mDNS.ino
+++ websocket_and_mDNS.ino
@@ -13,9 +13,6 @@ You will have to create arduino_secrets.h to store the Wifi Credentials (if usin
 
 
 *********** ToDo ***********
-- Update for FeatherS3
-  + Have to change the LED over to using the IO ports instead of the onboard LED (although the FeatherS3 does have one, but it's a different flavor of controller)
-  + FeatherS3 also does not use the DotStar LED controller
 - Add Support for the Ethernet Wing
 - Add Support for Relay Wing control
 - Need to investigate ArduinoJSON to see if there is a way to list all included keys before trying to read value
@@ -28,8 +25,11 @@ You will have to create arduino_secrets.h to store the Wifi Credentials (if usin
 
     {
       "control": {
-        "greenLED": "on",
-        "redLED": "off",
+        "redLED": "on",
+        "greenLED": "off",
+        "blueLED": "off",
+        "relay0": "off",
+        "display0": "Test Display Message"
       }
     }
 
@@ -41,14 +41,29 @@ You will have to create arduino_secrets.h to store the Wifi Credentials (if usin
 #include <ESPmDNS.h>
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
-#include <Adafruit_DotStar.h>
+#include <Adafruit_NeoPixel.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 #include "arduino_secrets.h"
 
 #define NUMPIXELS 1
-#define DATAPIN 40
-#define CLOCKPIN 45
-Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+#define PIN 40
+#define SDA 8
+#define SCL 9
+#define redLEDpin 18
+#define greenLEDpin 14
+#define blueLEDpin 12
+#define LDO2enable 39
+#define button 17
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 128 // OLED display height, in pixels
+#define OLED_RESET -1     // can set an oled reset pin if desired
+
+//Adafruit_SH1107 display = Adafruit_SH1107(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET, 1000000, 100000);
+
+Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 // Constants
 const char* ssid = SECRET_SSID; // Defined in arduino_secrets.h
@@ -110,19 +125,18 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
       // We need to pull the current LED color state and then add or remove color
       // strip.getPixelColor returns 32 bits of data, only which the low 24 bits are used
       // Here is the believed formatting:
-      // 0000 0000 GGGG GGGG RRRR RRRR BBBB BBBB
-      // Note the order of colors G and R and swapped (I don't know why it works this way)
+      // 0000 0000 RRRR RRRR GGGG GGGG BBBB BBBB
       // Each R, G or B value can be up to 255
       // We can use bit masking and shifting to play with this value!
-      // Green = 16 bit offset
-      // Red = 8 bit offset
+      // Red = 16 bit offset
+      // Green = 8 bit offset
       // Blue = 0 bit offset
       //
-      // So to turn on green, we need to shift 0xFF to the left by 16 bits and OR it with register
-      // To turn off green, we need to AND 0x00FFFF with the register
-      //
-      // To turn on red, we shift 0xFF to the left by 8 bits and OR it with register
+      // To turn on red, we shift 0xFF to the left by 16 bits and OR it with register
       // To turn off red, we need to AND 0xFF00FF with the register
+      //
+      // So to turn on green, we need to shift 0xFF to the left by 8 bits and OR it with register
+      // To turn off green, we need to AND 0x00FFFF with the register
       //
       // To turn on blue, we just OR the register with 0xFF
       // To turn blue off, AND the register with 0xFFFF00
@@ -132,27 +146,40 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
       uint32_t color = strip.getPixelColor(0);
       
       if (strcmp(greenLED, LEDon) == 0){
-        color = color | (0xFF<<16);
-        strip.setPixelColor(0, color);
-        Serial.println("Green LED On!");
-      }
-      if (strcmp(redLED, LEDon) == 0){
         color = color | (0xFF<<8);
         strip.setPixelColor(0, color);
+        Serial.println("Green LED On!");
+        //display.clearDisplay();
+        //display.setCursor(0,0);             // Start at top-left corner
+        //display.setTextSize(2);             // Draw 2X-scale text
+        //display.setTextColor(SH110X_WHITE);
+        //display.println("LED:");
+        //display.println("Green");
+        //display.display();
+      }
+      if (strcmp(redLED, LEDon) == 0){
+        color = color | (0xFF<<16);
+        strip.setPixelColor(0, color);
         Serial.println("Red LED On!");
+        //display.clearDisplay();
+        //display.setCursor(0,0);             // Start at top-left corner
+        //display.setTextSize(2);             // Draw 2X-scale text
+        //display.setTextColor(SH110X_WHITE);
+        //display.println("LED:");
+        //display.println("Red");
+        //display.display();
       } 
       if (strcmp(greenLED, LEDoff) == 0){
-        color = color & 0x00FFFF;
+        color = color & 0xFF00FF;
         strip.setPixelColor(0, color);
         Serial.println("Green LED Off!");
       }
       if (strcmp(redLED, LEDoff) == 0){
-        color = color & 0xFF00FF;
+        color = color & 0x00FFFF;
         strip.setPixelColor(0, color);
         Serial.println("Red LED Off!");
       } 
-      
-      strip.show();
+     strip.show(); 
    }
       //webSocket.sendTXT(num, payload);
       webSocket.broadcastTXT(payload);
@@ -174,11 +201,30 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
 
 void setup() {
 
-  pinMode(4, INPUT);
-
   // Start Serial port
   Serial.begin(115200);
-  delay(10000);
+
+  Wire.begin(SDA, SCL);
+  
+  //display.begin(0x3D, true); // Address 0x3D default
+  //display.display();
+  //display.clearDisplay();
+  // initialize digital pin LED_BUILTIN as an output.
+  //pinMode(4, INPUT);
+  //pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+  pinMode(blueLEDpin, OUTPUT);
+  pinMode(LDO2enable, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+
+  digitalWrite(redLEDpin, HIGH);
+  digitalWrite(greenLEDpin, HIGH);
+  digitalWrite(blueLEDpin, HIGH);
+  delay(100);
+  digitalWrite(LDO2enable, HIGH);
+  delay(100);
+
   // Connect to access point
   Serial.println("Connecting");
   WiFi.begin(ssid, password);
@@ -212,9 +258,8 @@ void setup() {
         // Add service to MDNS-SD
     MDNS.addService("http", "tcp", 80);
 
-  strip.begin();
-  strip.show();
-
+strip.begin();
+strip.clear();
 
 }
 
@@ -224,15 +269,15 @@ void loop() {
   // {"event":{"button0":"pressed"}}
 
   //Serial.println(analogRead(4));
-  if (analogRead(4) < 1000) { //Until we get a button wired in, we're using the on-board ambient light sensor as an input for this testing.
-    messageCounter++;
+  //if (analogRead(4) < 1000) { //Until we get a button wired in, we're using the on-board ambient light sensor as an input for this testing.
+  //  messageCounter++;
      
-    if (messageCounter > 5000) {
+    if (!digitalRead(button)) {
+      delay(250);
       //webSocket.broadcastTXT("Hey! Who turned out the lights?!");
       webSocket.broadcastTXT("{\"event\":{\"button0\":\"pressed\"}}");
-      messageCounter = 0;
+      //messageCounter = 0;
     }
-  }
   // Look for and handle WebSocket data
   webSocket.loop();
   
